b463a73e4204bf5750d45428d0a8e78843468753
Tom McLaughlin <tmclaugh@gmail.com>
2017-06-28 17:38:12 -0400

    Refactor as we get ready to add DynamoDB insertion

--- aws-adm-s3-retriever.py
+++ aws-adm-s3-retriever.py
@@ -15,9 +15,9 @@ if os.environ.get('DEBUG'):
     logging.root.setLevel(level=logging.DEBUG)
 _logger = logging.getLogger(__name__)
 
-def _extract_file_object(fileobj, filename):
+def _extract_zip_file_object(fileobj, filename):
     '''
-    Return an uncompressed file-like object.
+    Return an uncompressed file-like string object.
     '''
     zip_file = zipfile.ZipFile(fileobj)
     file_data = zip_file.read(filename)
@@ -26,9 +26,9 @@ def _extract_file_object(fileobj, filename):
     # We're dealing with CSV files so just convert to string here.
     return io.StringIO(file_data_string)
 
-def _fetch_object(bucket, key):
+def _fetch_s3_file_object(bucket, key):
     '''
-    Fetch a file from a URL and return a file-like object.
+    Fetch a file from a URL and return a file-like byte object.
     '''
     _logger.debug('Fetching object; Bucket={}, Key={}'.format(bucket, key))
     s3_client = boto3.client('s3')
@@ -39,6 +39,35 @@ def _fetch_object(bucket, key):
     zip_file = io.BytesIO(s3_object_body)
 
     return zip_file
+
+def _get_billing_data_from_event_record(record, start_line_item=None, end_line_item=None):
+    '''
+    Get billing data
+    '''
+    # This will be a list of OrderedDict items from the csv.DictReader()
+    _logger.debug('processing record: {}'.format(json.dumps(record)))
+    s3_info = record.get('s3')
+    s3_bucket = s3_info.get('bucket').get('name')
+    s3_key = s3_info.get('object').get('key')
+
+    billing_file_zip = _fetch_s3_file_object(s3_bucket, s3_key)
+    billing_file = _extract_zip_file_object(
+        billing_file_zip,
+        '.'.join(s3_key.split('.')[:-1])
+    )
+
+    billing_data_items = csv.DictReader(billing_file)
+    billing_data_items_list = []
+    for row in billing_data_items:
+        billing_data_items_list.append(row)
+
+    _logger.debug(
+        'billing data: {}'.format(
+            json.dumps(billing_data_items_list[start_line_item:end_line_item])
+        )
+    )
+
+    return billing_data_items_list
 
 def handler(event, context):
     '''
@@ -53,31 +82,14 @@ def handler(event, context):
     end_line_item = event.get('EndLineItem')
 
     # This will be a list of OrderedDict items from the csv.DictReader()
-    billing_data_total = []
+    billing_data_items_total = []
     for record in event_records:
-        _logger.debug('processing record: {}'.format(json.dumps(record)))
-        s3_info = record.get('s3')
-        s3_bucket = s3_info.get('bucket').get('name')
-        s3_key = s3_info.get('object').get('key')
-
-        billing_file_zip = _fetch_object(s3_bucket, s3_key)
-        billing_file = _extract_file_object(
-            billing_file_zip,
-            '.'.join(s3_key.split('.')[:-1])
+        billing_data_items = _get_billing_data_from_event_record(
+            record,
+            start_line_item,
+            end_line_item
         )
-        billing_data_items = csv.DictReader(billing_file)
 
-        billing_data = []
-        for row in billing_data_items:
-            billing_data.append(row)
+    billing_data_items_total.extend(billing_data_items)
 
-        _logger.debug(
-            'billing data: {}'.format(
-                json.dumps(billing_data[start_line_item:end_line_item])
-            )
-        )
-        billing_data_total.extend(billing_data)
-
-    return billing_data_total[start_line_item:end_line_item]
-
-
+    return billing_data_items_total
