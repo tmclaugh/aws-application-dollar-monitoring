2e19e99cab6c4ba1fa2267a9d21a4b17e4207439
Tom McLaughlin <tmclaugh@gmail.com>
2017-07-05 14:53:24 -0400

    Batch writes to DynamoDB using table context manager.

--- aws-adm-sns-to-dynamodb.py
+++ aws-adm-sns-to-dynamodb.py
@@ -17,6 +17,17 @@ _logger = logging.getLogger(__name__)
 
 DYNAMODB_TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME')
 
+def _sanitize_item(item):
+    '''
+    Handle value constraints of DynamoDB
+    '''
+    # DynamoDB can't have empty strings but csv.DictReader earlier in system
+    # uses '' for empty fields.
+    for key, value in item.items():
+        if value == '':
+            item[key] = None
+    return item
+
 def handler(event, context):
     '''
     Retrieve Billing data from SNS and add to DynamoDB Table.
@@ -26,30 +37,21 @@ def handler(event, context):
     # Start inserting DynamoDB entries
     dynamodb_resource = boto3.resource('dynamodb')
     dynamodb_table = dynamodb_resource.Table(DYNAMODB_TABLE_NAME)
-    dynamodb_client_responses = []
 
     for record in event.get('Records'):
         sns_data = record.get('Sns')
         sns_message_string = sns_data.get('Message')
         sns_messages = json.loads(sns_message_string)
 
-        # Debating batching messages upstream.
+        # FIXME: Debating batching messages upstream. Remove this if we decide
+        # this is the way forward.
         if type(sns_messages) == dict:
             sns_messages = [sns_messages]
 
-        for message in sns_messages:
-            # DynamoDB can't have empty strings but csv.DictReader uses '' for
-            # empty fields.
-            for key, value in message.items():
-                if value == '':
-                    message[key] = None
-            _logger.info('Sending item to DynamoDB: {}'.format(json.dumps(message)))
-            resp = dynamodb_table.put_item(Item=message)
-            _logger.debug(
-                'dynamodb response: {}'.format(
-                    json.dumps(resp)
-                )
-            )
-            dynamodb_client_responses.append(resp)
+        with dynamodb_table.batch_writer() as batch:
+            for message in sns_messages:
+                sanitized_message = _sanitize_item(message)
+                _logger.info('Sending item to DynamoDB: {}'.format(json.dumps(sanitized_message)))
+                batch.put_item(Item=message)
 
-    return dynamodb_client_responses
+    return
